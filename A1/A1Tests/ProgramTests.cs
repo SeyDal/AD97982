6b5978145a67cf77c09f2a216d49473a0d996e69
seyyedmehdi seyyedalhosseini <mehdi6583@gmail.com>
2019-02-23 00:08:11 +0330

    A1

---
+++ A1/A1Tests/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        
+    }
+}
